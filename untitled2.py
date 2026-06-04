485d9c8212c2034e2dcb485deca5d1923a02a843
gapaymykel8-blip <gapaymykel8@gmail.com>
2026-06-04 10:37:43 +0800

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -3,30 +3,94 @@ import numpy as np
 
 st.title('What will this look like?') 
 
-# Your data array
-cities = np.array(["New York", "London", "Tokyo", "Paris", "Sydney"])
-
-st.title("Fetch Data from an Array")
-
-# User selects an item from the array
-selected_city = st.selectbox("Choose a city:", cities)
-
-# Fetch and use the selected data
-st.write(f"You fetched: **{selected_city}**")
-
-st.markdown('Fun lets try with a dictionary')
-
-import streamlit as st
-import pandas as pd
 import numpy as np
-import time
+import pandas as pd
+import streamlit as st
 
-df = pd.DataFrame(np.random.randn(15, 3), columns=["A", "B", "C"])
-chart = st.line_chart(df)
+from faker import Faker
 
-for tick in range(10):
-    time.sleep(0.5)
-    new_row = pd.DataFrame(np.random.randn(1, 3), columns=["A", "B", "C"])
-    chart.add_rows(new_row)
+@st.cache_data
+def get_profile_dataset(number_of_items: int = 20, seed: int = 0) -> pd.DataFrame:
+    new_data = []
 
-st.button("Regenerate")
+    fake = Faker()
+    np.random.seed(seed)
+    Faker.seed(seed)
+
+    for i in range(number_of_items):
+        profile = fake.profile()
+        new_data.append(
+            {
+                "name": profile["name"],
+                "daily_activity": np.random.rand(25),
+                "activity": np.random.randint(2, 90, size=12),
+            }
+        )
+
+    profile_df = pd.DataFrame(new_data)
+    return profile_df
+
+
+column_configuration = {
+    "name": st.column_config.TextColumn(
+        "Name", help="The name of the user", max_chars=100, width="medium"
+    ),
+    "activity": st.column_config.LineChartColumn(
+        "Activity (1 year)",
+        help="The user's activity over the last 1 year",
+        width="large",
+        y_min=0,
+        y_max=100,
+    ),
+    "daily_activity": st.column_config.BarChartColumn(
+        "Activity (daily)",
+        help="The user's activity in the last 25 days",
+        width="medium",
+        y_min=0,
+        y_max=1,
+    ),
+}
+
+select, compare = st.tabs(["Select members", "Compare selected"])
+
+with select:
+    st.header("All members")
+
+    df = get_profile_dataset()
+
+    event = st.dataframe(
+        df,
+        column_config=column_configuration,
+        use_container_width=True,
+        hide_index=True,
+        on_select="rerun",
+        selection_mode="multi-row",
+    )
+
+    st.header("Selected members")
+    people = event.selection.rows
+    filtered_df = df.iloc[people]
+    st.dataframe(
+        filtered_df,
+        column_config=column_configuration,
+        use_container_width=True,
+    )
+
+with compare:
+    activity_df = {}
+    for person in people:
+        activity_df[df.iloc[person]["name"]] = df.iloc[person]["activity"]
+    activity_df = pd.DataFrame(activity_df)
+
+    daily_activity_df = {}
+    for person in people:
+        daily_activity_df[df.iloc[person]["name"]] = df.iloc[person]["daily_activity"]
+    daily_activity_df = pd.DataFrame(daily_activity_df)
+
+    if len(people) > 0:
+        st.header("Daily activity comparison")
+        st.bar_chart(daily_activity_df)
+        st.header("Yearly activity comparison")
+        st.line_chart(activity_df)
+    else:
+        st.markdown("No members selected.")
