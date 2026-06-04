89f1a17dfb6f2ce10ce092328e32d72bac774259
gapaymykel8-blip <gapaymykel8@gmail.com>
2026-06-04 10:19:17 +0800

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 
 st.title('What will this look like?') 
-st.markdown('Suggesting improvements to the streamlit app, that Sir Arcy developed') 
 
 # Your data array
 cities = np.array(["New York", "London", "Tokyo", "Paris", "Sydney"])
@@ -15,3 +14,14 @@ selected_city = st.selectbox("Choose a city:", cities)
 # Fetch and use the selected data
 st.write(f"You fetched: **{selected_city}**")
 
+st.markdown('Fun lets try with a dictionary')
+
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(
+    rng(0).standard_normal((50, 20)), columns=("col %d" % i for i in range(20))
+)
+
+st.dataframe(df)
