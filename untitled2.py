44af2594aa37298e2d0758b19bce60ecc2daf8cb
gapaymykel8-blip <gapaymykel8@gmail.com>
2026-06-04 15:58:33 +0800

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -18,8 +18,49 @@ yay = show_data(df)
 
 st.markdown("Going to try and make the tables selectable but caching seems to work, with csv files i dont see why it shouldnt work with the code") 
 
-st.text_input("Your name", key="name")
+import streamlit as st
+import pandas as pd
+import time
 
-# This exists now:
-st.session_state.name
+if "dfa" not in st.session_state:
+    st.session_state["dfa"] = pd.DataFrame(
+        {
+            "Par": ["Apple", "Strawberry", "Banana"],
+            "Cat1": ["good", "good", "bad"],
+            "Cat2": ["healthy", "healthy", "unhealthy"],
+            "Active": [False, False, False],
+        }
+    )
+
+
+def active_dfa():
+    return st.session_state["dfa"][st.session_state["dfa"]["Active"] == True].copy()
+
+
+def get_index(row):
+    return active_dfa().iloc[row].name
+
+
+def commit():
+    for row in st.session_state.editor["edited_rows"]:
+        row_index = get_index(row)
+        for key, value in st.session_state.editor["edited_rows"][row].items():
+            st.session_state["dfa"].at[row_index, key] = value
+
+
+st.header("Filter and edit data")
+name = st.text_input("Search for ...", on_change=commit)
+if name == "":
+    st.session_state["dfa"].Active = True
+else:
+    st.session_state["dfa"].Active = False
+    st.session_state["dfa"].loc[
+        st.session_state["dfa"]["Par"].str.contains(name, case=False), "Active"
+    ] = True
+
+edited_dfa = st.data_editor(
+    active_dfa(), column_order=["Par", "Cat1", "Cat2"], key="editor"
+)
+
+st.button("Save", on_click=commit)
 
