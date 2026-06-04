c939b29ca1c009146d549b770981db8ed55f1e4a
gapaymykel8-blip <gapaymykel8@gmail.com>
2026-06-04 15:17:39 +0800

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -11,11 +11,9 @@ df = pd.read_csv('Blue Spectra(in).csv')
 @st.cache_data
 def show_data(data):
     st.header("Data analysis")
-    st.success("Fetched data from API!")
-    st.write("Here is a plot of the data:")
     st.write("And here is the raw data:")
     st.dataframe(data)
 
 yay = show_data(df)
-yay
+
 
