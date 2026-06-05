29d5e865cc0e0539318589568f01734987c05f62
gapaymykel8-blip <gapaymykel8@gmail.com>
2026-06-05 14:02:04 +0800

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -70,6 +70,15 @@ event_time = st.datetime_input(
 )
 st.write("Event scheduled for", event_time)
 
+import datetime
+import streamlit as st
+
+event_time = st.datetime_input(
+    "Schedule your event",
+    datetime.datetime(2025, 11, 19, 16, 45),
+)
+st.write("Event scheduled for", event_time)
+
 
 
 
